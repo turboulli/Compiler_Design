ae63817907206fcb8f5ddc60e7b0233f05ec2de5
Sebastian Leininger <ueolm@student.kit.edu>
2025-05-13 21:22:06 +0200

    Add test.

--- src/test/java/edu/kit/kastel/vads/compiler/CompilerTest.java
+++ src/test/java/edu/kit/kastel/vads/compiler/CompilerTest.java
@@ -1,12 +1,28 @@
 package edu.kit.kastel.vads.compiler;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class CompilerTest {
+    String generateAssembly(String input) {
+        return "";
+    }
+
     @Test
     public void test() {
-        assertTrue(true);
+        String input = """
+        int main() {
+            return 0;
+        }
+        """;
+
+        String expectedOutput = """
+        function main {
+          %0 = const 0
+          ret %0
+        }""";
+
+        assertEquals(expectedOutput, generateAssembly(input));
     }
 }

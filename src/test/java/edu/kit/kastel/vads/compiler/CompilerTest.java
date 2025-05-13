16bdab6b471b5090bdf52f44784ddc2fc0c66067
Sebastian Leininger <ueolm@student.kit.edu>
2025-05-13 14:51:31 +0200

    Add test.

---
+++ src/test/java/edu/kit/kastel/vads/compiler/CompilerTest.java
@@ -0,0 +1,12 @@
+package edu.kit.kastel.vads.compiler;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CompilerTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}

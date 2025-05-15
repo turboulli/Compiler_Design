01eba148df009cf19644ff8615f4cdf24c1fe637
Sebastian Leininger <ueolm@student.kit.edu>
2025-05-15 06:47:40 +0200

    Add example.

---
+++ examples/fibonacci.c
@@ -0,0 +1,23 @@
+int main() {
+    int f1 = 1;
+    int f2 = 1;
+    int f3 = f2 + f1;
+    int f4 = f3 + f2;
+    int f5 = f4 + f3;
+    int f6 = f5 + f4;
+    int f7 = f6 + f5;
+    int f8 = f7 + f6;
+    int f9 = f8 + f7;
+    int f10 = f9 + f8;
+    int f11 = f10 + f9;
+    int f12 = f11 + f10;
+    int f13 = f12 + f11;
+    int f14 = f13 + f12;
+    int f15 = f14 + f13;
+    int f16 = f15 + f14;
+    int f17 = f16 + f15;
+    int f18 = f17 + f16;
+    int f19 = f18 + f17;
+    int f20 = f19 + f18;
+    return f20;
+}
